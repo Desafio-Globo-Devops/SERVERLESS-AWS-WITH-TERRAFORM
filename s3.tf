425ae835ae3cb2208afcb6afa878862c2958f8c8
danielcrash <daniel.silva@fourlog.com>
2020-07-09 11:08:43 -0400

    remove path

--- s3.tf
+++ s3.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.dynamo.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/${var.account_id}/CloudTrail/${var.region}/2020/07/07"
+    filter_prefix       = "AWSLogs/${var.account_id}/CloudTrail/${var.region}/"
     filter_suffix       = ".json.gz"
   }
     depends_on = [aws_lambda_permission.allow_bucket]
